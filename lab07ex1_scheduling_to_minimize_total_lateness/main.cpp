9941da13b71f58d88373cc76f957efb4f3e756aa
Zhong Zhenyu <nczzy1997@gmail.com>
2020-05-13 22:34:35 +0800

    Add solution code to code lab #7 ex1

---
+++ lab07ex1_scheduling_to_minimize_total_lateness/main.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <limits>
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<int, std::pair<int, int>>> assignments(n);
+    for (auto i = 0; i < n; ++i) {
+        assignments[i].first = i;
+        std::cin >> assignments[i].second.first >> assignments[i].second.second;
+    }
+    auto minLateness = std::numeric_limits<int>::max();
+    do {
+        auto timePassed = 0;
+        auto lateness = 0;
+        for (const auto &assignment : assignments) {
+            timePassed += assignment.second.first;
+            lateness += std::max(0, timePassed - assignment.second.second);
+        }
+        minLateness = std::min(minLateness, lateness);
+    } while (next_permutation(begin(assignments), end(assignments)));
+    std::cout << minLateness << std::endl;
+    return 0;
+}
