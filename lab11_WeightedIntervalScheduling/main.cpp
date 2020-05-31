26c3b1b111fe9ebce2021865ca38a4d9369762e6
Zhong Zhenyu <nczzy1997@gmail.com>
2020-05-31 18:19:12 +0800

    Remove comments

--- lab11_WeightedIntervalScheduling/main.cpp
+++ lab11_WeightedIntervalScheduling/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <algorithm>
 
-// A job has start time, finish time and profit.
 class Job {
 public:
     int start;
@@ -10,20 +9,13 @@ public:
     int profit;
 };
 
-// A utility function that is used for sorting events according to finish time.
 bool operator<(const Job &lhs, const Job &rhs) {
     return lhs.finish < rhs.finish;
 }
 
-// A Binary Search based function to find the latest job (before current job) that doesn't conflict with current
-// job. "index" is index of the current job. This function returns -1 if all jobs before index conflict with it.
-// The vector jobs is sorted in increasing order of finish time.
 inline int binarySearch(const std::vector<Job> &jobs, const int index) {
-    // Initialize 'lo' and 'hi' for Binary Search.
     auto lo = 0;
     auto hi = index - 1;
-
-    // Perform binary Search iteratively.
     while (lo <= hi) {
         const auto mid = (lo + hi) / 2;
         if (jobs.at(mid).finish <= jobs.at(index).start) {
@@ -39,30 +31,20 @@ inline int binarySearch(const std::vector<Job> &jobs, const int index) {
 }
 
 int weightedIntervalScheduling(std::vector<Job> &jobs) {
-    // Sort jobs according to finish time.
     sort(begin(jobs), end(jobs));
-
-    // Create an array to store solutions of sub-problems. table[i]
-    // stores the profit for jobs till arr[i] (including arr[i]).
     std::vector<int> table(jobs.size());
     table.at(0) = jobs.at(0).profit;
-
-    // Fill entries in table using recursive property.
     for (auto i = 1; i < jobs.size(); ++i) {
-        // Find profit including the current job.
         auto profit = jobs.at(i).profit;
         const auto latestJob = binarySearch(jobs, i);
         if (latestJob != -1) {
             profit += table.at(latestJob);
         }
-
-        // Store maximum of including and excluding the current job.
         table.at(i) = std::max(profit, table.at(i - 1));
     }
     return table.at(jobs.size() - 1);
 }
 
-// The main function that prints the maximum possible profit from given jobs.
 int main() {
     int n;
     std::cin >> n;
